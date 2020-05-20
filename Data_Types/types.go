f582fba4ae55816025ac18dfca6dc0203cc149bc
MBennettLowe <mbennettlowe@gmail.com>
2020-05-19 20:23:39 -0400

    Go/Python Data Types

---
+++ Data_Types/types.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+// STRING	
+nickname := "Pooh"
+
+// INTEGER
+money_in_checking := 14
+
+// FLOAT
+money_in_savings := 3.50
+
+// BOOLEAN
+am_i_broke := true
+
+
+fmt.Printf("%T\n",nickname)
+fmt.Printf("%T",money_in_checking)
+fmt.Printf("%T",money_in_savings)
+fmt.Printf("%T",am_i_broke)
+
+}
---
+++ Data_Types/types.py
@@ -0,0 +1,16 @@
+# We are playing with data types
+
+# STRING    
+nickname = "Pooh"
+
+# INTEGER
+money_in_checking = 14
+money_in_savings = 3.50
+
+# Boolean
+am_i_broke = True
+
+print(type(nickname))
+print(type(money_in_checking))
+print(type(money_in_savings))
+print(type(am_i_broke))
